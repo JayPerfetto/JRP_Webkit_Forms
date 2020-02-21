c0e26f4e5c1db98e1400e2f18f9efc1f07e4333e
Jason Perfetto <perfett_jaso@bentley.edu>
2020-02-21 14:55:21 -0500

    login providers

--- src/app/pages/content-pages/content-pages.module.ts
+++ src/app/pages/content-pages/content-pages.module.ts
@@ -18,7 +18,13 @@ import { FirebaseUIModule, firebase, firebaseui } from 'firebaseui-angular';
 const firebaseUiAuthConfig: firebaseui.auth.Config = {
     signInFlow: 'popup',
     signInOptions: [
+      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID,
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
     ],
     tosUrl: '<your-tos-link>',
     privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
